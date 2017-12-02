0b0c6bd3aa1f719bd6cc5fd53280f83fefadb7d7
rkuchar <kucharrom@gmail.com>
2017-12-02 17:03:00 +0100

    welcome page for server API

--- api/routes/todoListRoutes.js
+++ api/routes/todoListRoutes.js
@@ -11,4 +11,11 @@ module.exports = function (app) {
         .get(todoList.read_a_task)
         .put(todoList.update_a_task)
         .delete(todoList.delete_a_task);
+
+    app.route("")
+        .get(function(req, res) {
+            var data = "<h1>Pub tracker API</h1></br><p>Find more information at <a href='https://github.com/kucharo2/via_project'>project repository</a>.</p>";
+            res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
+            res.write(data);
+        })
 };
